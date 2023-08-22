41f60c3135131a5dd3190677c17d441b36ff3f4e
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-22 09:51:23 +0330

    Add files via upload

---
+++ ConsoleApp102/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp102
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int total = 0;
+
+            for (int counter = 2; counter <= 20; counter += 2)
+            {
+                total += counter;
+            }
+            Console.WriteLine($"sum is{total}");
+            Console.ReadLine();
+        }
+    }
+}
